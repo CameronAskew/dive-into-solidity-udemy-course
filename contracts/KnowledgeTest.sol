cd65f74f08cba3e63e51bd80527be97d01a4e50b
Askew Mac <askew@Askews-MacBook-Pro.local>
2022-08-25 16:23:36 -0400

    complete knowledge test & lottery projects

--- contracts/KnowledgeTest.sol
+++ contracts/KnowledgeTest.sol
@@ -3,11 +3,37 @@
 pragma solidity >=0.5.0 <0.9.0;
 
 contract KnowledgeTest {
+
     string[] public tokens = ["BTC", "ETH"];
     address[] public players;
+    address public immutable owner;
 
-    function changeTokens() public view {
-        string[] memory t = tokens;
+    constructor() {
+        owner = msg.sender;
+    }
+
+    receive() external payable {}
+
+    function changeTokens() public {
+        string[] storage t = tokens;
         t[0] = "VET";
     }
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function transferAll(address payable recipient) public  {
+        require(msg.sender == owner, "ONLY_OWNER");
+        (bool sent, bytes memory data) = recipient.call{value: address(this).balance}("");
+    }
+
+    function start() public {
+        players.push(msg.sender);
+    }
+
+    function concatenate(string calldata s1, string calldata s2) public pure returns (string memory) {
+        return string.concat(s1, s2);
+    }
 }
+
--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -7,40 +7,35 @@ contract Lottery {
     // declaring the state variables
     address[] public players; //dynamic array of type address payable
     address[] public gameWinners;
-    address public owner;
+    address public immutable owner;
 
     // declaring the constructor
     constructor() {
-        // TODO: initialize the owner to the address that deploys the contract
+        owner = msg.sender;
     }
 
     // declaring the receive() function that is necessary to receive ETH
     receive() external payable {
-        // TODO: require each player to send exactly 0.1 ETH
-        // TODO: append the new player to the players array
+        require(msg.value == 0.1 ether, "INCORRECT_ETH_VALUE");
+        players.push(msg.sender);
     }
 
     // returning the contract's balance in wei
     function getBalance() public view returns (uint256) {
-        // TODO: restrict this function so only the owner is allowed to call it
-        // TODO: return the balance of this address
+        require(msg.sender == owner, "ONLY_OWNER");
+        return address(this).balance;
     }
 
     // selecting the winner
     function pickWinner() public {
-        // TODO: only the owner can pick a winner 
-        // TODO: owner can only pick a winner if there are at least 3 players in the lottery
+        require(msg.sender == owner, "ONLY_OWNER");
+        require(players.length >= 3, "NOT_ENOUGH_PLAYERS");
 
         uint256 r = random();
-        address winner;
-
-        // TODO: compute an unsafe random index of the array and assign it to the winner variable 
-
-        // TODO: append the winner to the gameWinners array
-
-        // TODO: reset the lottery for the next round
-
-        // TODO: transfer the entire contract's balance to the winner
+        address winner = players[r % players.length];
+        gameWinners.push(winner);
+        delete players;
+        (bool sent, bytes memory data) = winner.call{value: address(this).balance}("");
     }
 
     // helper function that returns a big random integer
@@ -59,3 +54,4 @@ contract Lottery {
             );
     }
 }
+
